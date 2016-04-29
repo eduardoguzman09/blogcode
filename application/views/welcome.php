8acc457bcf6094e512f1d8dd9f42d494b53713ec
Lazaro <lazaro.guzman@clickit.com>
2016-04-29 16:34:11 -0500

    agregado de codeigniter

---
+++ application/views/welcome.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Primera vista lazaro</title>
+</head>
+<body>
+<div class="container">
+	<center>
+	<h1>Bienvenidos</h1>
+	<h2>Bienvenidos</h2>
+	<h3>Bienvenidos</h3>
+	<h4>Bienvenidos</h4>
+	<h5>Bienvenidos</h5>
+	<h6>Bienvenidos</h6>
+</center>
+</div>
+	
+</body>
+</html>
